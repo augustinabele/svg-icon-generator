6ffb97abafbde97432b406ad7d48608b28fb821a
Augustin Abelé <aabele@yoobic.com>
2023-03-12 21:20:44 +0100

    chore: add astro vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,21 @@ import presetWind from "@unocss/preset-wind";
 import presetWebFonts from "@unocss/preset-web-fonts";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
+  integrations: [unocss({
+    presets: [presetWind(), presetWebFonts({
+      provider: "google",
+      fonts: {
+        mono: "Space Mono"
+      }
+    })],
+    safelist: [
+      /* this you can use to exclude utilities from purge */
+    ]
+  })],
   output: "server",
-  integrations: [
-    unocss({
-      presets: [
-        presetWind(),
-        presetWebFonts({
-          provider: "google",
-          fonts: {
-            mono: "Space Mono",
-          },
-        }),
-      ],
-      safelist: [
-        /* this you can use to exclude utilities from purge */
-      ],
-    }),
-  ],
+  adapter: vercel()
 });
