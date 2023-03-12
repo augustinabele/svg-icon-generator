82fbcfdf93399a5d34be18e4f959900d535e1fc8
Augustin Abelé <aabele@yoobic.com>
2023-03-12 19:21:58 +0100

    fix: load data from endpoints

--- src/pages/api/generate/[prompt].ts
+++ src/pages/api/generate/[prompt].ts
@@ -1,19 +1,32 @@
-const getIcon = (prompt: string) => {
-  return '<svg>' + prompt + '</svg>';
+import axios from 'axios';
+
+const fetchIcons = async (prompt: string): Promise<[]> => {
+  return axios.post('https://europe-west1-scrypr.cloudfunctions.net/createSVG', {
+    'prompt': prompt,
+  })
+  .then(function (response) {
+    console.log(response);
+    return response.data;
+  })
+  .catch(function (error) {
+    console.log(error);
+    return error;
+  });
 }
 
 export async function get({ params }) {
   const prompt = params.prompt;
-  const icon = await getIcon(prompt);
 
-  if (!icon) {
+  const data = await fetchIcons(prompt);
+
+  if (!data) {
     return new Response(null, {
       status: 404,
       statusText: 'Not found'
     });
   }
 
-  return new Response(JSON.stringify(icon), {
+  return new Response(JSON.stringify(data), {
     status: 200,
     headers: {
       "Content-Type": "application/json"
